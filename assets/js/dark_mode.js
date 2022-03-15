062f0e02188b5ff450839a0598f296db9c83a163
nathansam <nathansam@users.noreply.github.com>
2022-03-15 14:42:40 +0000

    deploy: e18c508d85fee6aae47fb495f06b1320eaa200cc

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -1,8 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const mode_toggle = document.getElementById("light-toggle");
-
-    mode_toggle.addEventListener("click", function() {
-        toggleTheme(localStorage.getItem("theme"));
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        setTheme("dark");
+        document.addEventListener('DOMContentLoaded', function() {
+            const mode_toggle = document.getElementById("light-toggle");
+        
+            mode_toggle.addEventListener("load", function() {
+                toggleTheme(localStorage.getItem("theme"));
+            });
+            mode_toggle.addEventListener("click", function() {
+                toggleTheme(localStorage.getItem("theme"));
+            });
+        });
+        
+  } else {
+    document.addEventListener('DOMContentLoaded', function() {
+        const mode_toggle = document.getElementById("light-toggle");
+    
+        mode_toggle.addEventListener("click", function() {
+            toggleTheme(localStorage.getItem("theme"));
+        });
     });
-});
+  }
 
+  // Whenever the user explicitly chooses to respect the OS preference
+  localStorage.removeItem('theme');
